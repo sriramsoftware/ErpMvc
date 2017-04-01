1acd241c4461ff1c7df9bf7bbe327a97337bf34e
elvis <elvis.crego@gmail.com>
2017-04-01 08:12:56 -0400

    Cambiando menu para que se le pongan  agregados

--- ErpMvc/Controllers/MenusController.cs
+++ ErpMvc/Controllers/MenusController.cs
@@ -44,6 +44,13 @@ namespace ErpMvc.Controllers
             return Json(e, JsonRequestBehavior.AllowGet);
         }
 
+        public JsonResult AgregadosData(int id)
+        {
+            var elaboracion = _elaboracionService.Elaboraciones().Find(id);
+            var agregados = elaboracion.Agregados.Select(a => new {Id = a.Id, Nombre = a.Producto.Nombre, Costo = a.Costo}).ToList();
+            return Json(agregados, JsonRequestBehavior.AllowGet);
+        }
+
         [Authorize(Roles = RolesMontin.UsuarioAvanzado + "," + RolesMontin.Administrador)]
         public ActionResult AgregarMenu()
         {
@@ -107,14 +114,20 @@ namespace ErpMvc.Controllers
             return View(elab);
         }
 
+        public ActionResult Agregados(int id)
+        {
+            var elab = _elaboracionService.Elaboraciones().Find(id);
+            return View(elab);
+        }
+
         public PartialViewResult FichaDeCostoResumen(int id)
         {
             var elab = _elaboracionService.Elaboraciones().Include(e => e.Productos).SingleOrDefault(e => e.Id == id);
-            var costos = new Dictionary<int,decimal>();
+            var costos = new Dictionary<int, decimal>();
             foreach (var detalle in elab.Productos)
             {
-                var costo =_elaboracionService.GetPrecioDeProducto(detalle.ProductoId, detalle.UnidadDeMedidaId);
-                costos.Add(detalle.ProductoId,costo);
+                var costo = _elaboracionService.GetPrecioDeProducto(detalle.ProductoId, detalle.UnidadDeMedidaId);
+                costos.Add(detalle.ProductoId, costo);
             }
             ViewBag.Costos = costos;
             return PartialView("_TablaFichaDeCostoPartial", elab);
@@ -128,13 +141,33 @@ namespace ErpMvc.Controllers
             return Json(true, JsonRequestBehavior.AllowGet);
         }
 
+        [HttpPost]
+        [Authorize(Roles = RolesMontin.UsuarioAvanzado + "," + RolesMontin.Administrador)]
+        public JsonResult AgregarAgregadoAMenu(int menuId, int productoId, int unidadId, decimal cantidad, decimal precio)
+        {
+            _elaboracionService.AgregarAgregadoAElaboracion(menuId, productoId, cantidad, unidadId, precio);
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+
         public JsonResult ProductosEnMenu(int menuId)
         {
             var elaboracion = _elaboracionService.Elaboraciones().Find(menuId);
             var productos = new List<dynamic>();
             foreach (var prod in elaboracion.Productos)
             {
-                productos.Add(new {Producto = prod.Producto.Nombre, ProductoId = prod.ProductoId, Unidad = prod.UnidadDeMedida.Siglas, Cantidad = prod.Cantidad});
+                productos.Add(new { Producto = prod.Producto.Nombre, ProductoId = prod.ProductoId, Unidad = prod.UnidadDeMedida.Siglas, Cantidad = prod.Cantidad });
+            }
+
+            return Json(productos, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult AgregadosEnMenu(int menuId)
+        {
+            var elaboracion = _elaboracionService.Elaboraciones().Find(menuId);
+            var productos = new List<dynamic>();
+            foreach (var prod in elaboracion.Agregados)
+            {
+                productos.Add(new { Producto = prod.Producto.Nombre, ProductoId = prod.ProductoId, Unidad = prod.UnidadDeMedida.Siglas, Cantidad = prod.Cantidad });
             }
 
             return Json(productos, JsonRequestBehavior.AllowGet);
@@ -147,7 +180,7 @@ namespace ErpMvc.Controllers
             var menusLista = new List<dynamic>();
             foreach (var menu in menus)
             {
-                menusLista.Add(new { Id = menu.Id, Nombre = menu.Nombre , Precio = menu.PrecioDeVenta});
+                menusLista.Add(new { Id = menu.Id, Nombre = menu.Nombre, Precio = menu.PrecioDeVenta });
             }
 
             return Json(menusLista, JsonRequestBehavior.AllowGet);
@@ -158,6 +191,14 @@ namespace ErpMvc.Controllers
         public JsonResult QuitarProductoDeMenu(int menuId, int productoId)
         {
             _elaboracionService.QuitarProductoDeElaboracion(menuId, productoId);
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = RolesMontin.UsuarioAvanzado + "," + RolesMontin.Administrador)]
+        public JsonResult QuitarAgregadoDeMenu(int menuId, int productoId)
+        {
+            _elaboracionService.QuitarAgregadoDeElaboracion(menuId, productoId);
             return Json(true, JsonRequestBehavior.AllowGet);
         }
 
@@ -197,7 +238,7 @@ namespace ErpMvc.Controllers
                 TempData["exito"] = "Menu eliminado correctamente";
                 return RedirectToAction("Listado");
             }
-            
+
             TempData["error"] = "No se pudo eliminar el menu.";
             return RedirectToAction("Listado");
         }
