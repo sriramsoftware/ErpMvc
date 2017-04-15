250b3fa8938a68c923e35d56d9b3ecd3d996fe95
elvis <elvis.crego@gmail.com>
2017-04-14 20:38:00 -0400

    Haciendo operaciones contables cuando se gestionan otros gastos

--- ErpMvc/Controllers/OtrosGastosController.cs
+++ ErpMvc/Controllers/OtrosGastosController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using ContabilidadBL;
 using ContabilidadCore.Models;
 using ErpMvc.Models;
+using Microsoft.AspNet.Identity;
 
 namespace ErpMvc.Controllers
 {
@@ -16,17 +17,28 @@ namespace ErpMvc.Controllers
     {
         private DbContext _db;
         private PeriodoContableService _periodoContableService;
+        private SubmayorService _submayorService;
+        private CuentasServices _cuentasServices;
 
         public OtrosGastosController(DbContext context)
         {
             _db = context;
             _periodoContableService = new PeriodoContableService(context);
+            _submayorService = new SubmayorService(context);
+            _cuentasServices = new CuentasServices(context);
         }
 
         // GET: OtrosGastos
         public ActionResult Index()
         {
-            return View(_db.Set<OtrosGastos>().ToList());
+            var diaContableId = _periodoContableService.GetDiaContableActual().Id;
+            return View(_db.Set<OtrosGastos>().Where(g => g.DiaContableId == diaContableId).ToList());
+        }
+
+
+        public ActionResult Historico()
+        {
+            return View(_db.Set<OtrosGastos>().OrderBy(g => g.DiaContableId).ToList());
         }
 
         [Authorize(Roles = RolesMontin.UsuarioAvanzado + "," + RolesMontin.Administrador)]
@@ -45,10 +57,22 @@ namespace ErpMvc.Controllers
             if (ModelState.IsValid)
             {
                 _db.Set<OtrosGastos>().Add(otrosGastos);
-                _db.SaveChanges();
-                TempData["exito"] = "Gasto agregado correctamente";
+                var cuentaCaja = _cuentasServices.FindCuentaByNombre("Caja");
+                var cuentaGasto = _cuentasServices.FindCuentaByNombre("Gastos");
+                var concepto = _db.Set<ConceptoDeGasto>().Find(otrosGastos.ConceptoDeGastoId);
+                string detalle = "Pago de: " + concepto.Nombre;
+                if (_submayorService.AgregarOperacion(cuentaCaja.Id, cuentaGasto.Id, otrosGastos.Importe, DateTime.Now,
+                    detalle, User.Identity.GetUserId()))
+                {
+                    TempData["exito"] = "Gasto agregado correctamente";
+                }
+                else
+                {
+                    TempData["error"] = "Error al agregar el gasto";
+                }
                 return RedirectToAction("Index");
             }
+            ViewBag.DiaContable = _periodoContableService.GetDiaContableActual();
             ViewBag.ConceptoDeGastoId = new SelectList(_db.Set<ConceptoDeGasto>(), "Id", "Nombre", otrosGastos.ConceptoDeGastoId);
             return View(otrosGastos);
         }
@@ -67,9 +91,27 @@ namespace ErpMvc.Controllers
         {
             if (ModelState.IsValid)
             {
-                _db.Entry(otrosGastos).State = EntityState.Modified;
-                _db.SaveChanges();
-                TempData["exito"] = "Gasto modificado correctamente";
+                var entry = _db.Entry(otrosGastos);
+                entry.State = EntityState.Modified;
+                var importeAnterior = (decimal)entry.GetDatabaseValues()["Importe"];
+
+                var cuentaCaja = _cuentasServices.FindCuentaByNombre("Caja");
+                var cuentaGasto = _cuentasServices.FindCuentaByNombre("Gastos");
+                var concepto = _db.Set<ConceptoDeGasto>().Find(otrosGastos.ConceptoDeGastoId);
+
+                string detalle = "Pago de: " + concepto.Nombre;
+                _db.Set<Asiento>().Add(_submayorService.CrearAsientoContable(cuentaGasto.Id, cuentaCaja.Id, importeAnterior, DateTime.Now,
+                    "Ajuste por error en gasto " + otrosGastos.Id + " de " + concepto.Nombre, User.Identity.GetUserId()));
+                _db.Set<Asiento>().Add(_submayorService.CrearAsientoContable(cuentaCaja.Id, cuentaGasto.Id, otrosGastos.Importe, DateTime.Now,
+                    detalle, User.Identity.GetUserId()));
+                if (_submayorService.GuardarCambios())
+                {
+                    TempData["exito"] = "Gasto modificado correctamente";
+                }
+                else
+                {
+                    TempData["error"] = "Error al modificar el gasto";
+                }
                 return RedirectToAction("Index");
             }
             ViewBag.ConceptoDeGastoId = new SelectList(_db.Set<ConceptoDeGasto>(), "Id", "Nombre", otrosGastos.ConceptoDeGastoId);
@@ -106,8 +148,19 @@ namespace ErpMvc.Controllers
                 return new HttpNotFoundResult();
             }
             _db.Set<OtrosGastos>().Remove(gasto);
-            _db.SaveChanges();
-            TempData["exito"] = "Gasto eliminado correctamente";
+            var cuentaCaja = _cuentasServices.FindCuentaByNombre("Caja");
+            var cuentaGasto = _cuentasServices.FindCuentaByNombre("Gastos");
+            var concepto = _db.Set<ConceptoDeGasto>().Find(gasto.ConceptoDeGastoId);
+
+            if (_submayorService.AgregarOperacion(cuentaGasto.Id, cuentaCaja.Id, gasto.Importe, DateTime.Now,
+                "Ajuste por error en gasto " + gasto.Id + " de " + concepto.Nombre, User.Identity.GetUserId()))
+            {
+                TempData["exito"] = "Gasto eliminado correctamente";
+            }
+            else
+            {
+                TempData["error"] = "No se pudo eliminar el gasto";
+            }
             return RedirectToAction("Index");
         }
 
