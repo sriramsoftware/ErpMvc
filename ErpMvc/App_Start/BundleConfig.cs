b47a184ea79dcc2e16aef722457a4ee274595cf1
elvis <elvis.crego@gmail.com>
2017-05-29 21:12:39 -0400

    Actualizacion de scripts chosen

--- ErpMvc/App_Start/BundleConfig.cs
+++ ErpMvc/App_Start/BundleConfig.cs
@@ -19,8 +19,8 @@ namespace ErpMvc
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrap.min.js",
+                      "~/Scripts/respond.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
