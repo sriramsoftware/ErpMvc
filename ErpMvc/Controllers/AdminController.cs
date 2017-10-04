7e1976ddbb11fd0f000123f857e6ec6a013dda38
elvis <elvis.crego@gmail.com>
2017-10-04 15:17:42 -0400

    Acciona para borrar datos y crear entradas con la existencia de los productos

---
+++ ErpMvc/Controllers/AdminController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AlmacenCore.Models;
+using CompraVentaCore.Models;
+using ContabilidadCore.Models;
+using ErpMvc.Models;
+using Microsoft.AspNet.Identity;
+
+namespace ErpMvc.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult DeleteInfo()
+        {
+            var db = new ErpContext();
+            db.Set<DetalleDeCompra>().RemoveRange(db.Set<DetalleDeCompra>());
+            db.Set<Compra>().RemoveRange(db.Set<Compra>());
+
+            db.Set<EntradaAlmacen>().RemoveRange(db.Set<EntradaAlmacen>());
+
+            db.Set<DetalleSalidaAlmacen>().RemoveRange(db.Set<DetalleSalidaAlmacen>());
+            db.Set<ValeSalidaDeAlmacen>().RemoveRange(db.Set<ValeSalidaDeAlmacen>());
+            db.Set<SalidaPorMerma>().RemoveRange(db.Set<SalidaPorMerma>());
+
+            db.Set<MovimientoDeProducto>().RemoveRange(db.Set<MovimientoDeProducto>());
+
+            db.Set<OrdenPorDetalle>().RemoveRange(db.Set<OrdenPorDetalle>());
+            db.Set<AgregadoDeComanda>().RemoveRange(db.Set<AgregadoDeComanda>());
+            db.Set<DetalleDeComanda>().RemoveRange(db.Set<DetalleDeComanda>());
+            db.Set<Orden>().RemoveRange(db.Set<Orden>());
+            db.Set<Comanda>().RemoveRange(db.Set<Comanda>());
+
+            db.Set<AgregadosVendidos>().RemoveRange(db.Set<AgregadosVendidos>());
+            db.Set<DetalleDeVenta>().RemoveRange(db.Set<DetalleDeVenta>());
+            db.Set<Venta>().RemoveRange(db.Set<Venta>());
+            db.Set<Propina>().RemoveRange(db.Set<Propina>());
+
+            db.Set<Asiento>().RemoveRange(db.Set<Asiento>());
+            db.Set<DiaContable>().RemoveRange(db.Set<DiaContable>());
+
+            var dia = new DiaContable() { Abierto = true, Fecha = DateTime.Now };
+            db.Set<DiaContable>().Add(dia);
+
+            var existencias = db.Set<ExistenciaAlmacen>().ToList();
+
+            foreach (var existencia in existencias)
+            {
+                if (existencia.ExistenciaEnAlmacen > 0)
+                {
+                    db.Set<EntradaAlmacen>()
+                        .Add(new EntradaAlmacen()
+                        {
+                            Fecha = DateTime.Now,
+                            DiaContable = dia,
+                            AlmacenId = existencia.AlmacenId,
+                            ProductoId = existencia.ProductoId,
+                            UsuarioId = User.Identity.GetUserId(),
+                            Cantidad = existencia.ExistenciaEnAlmacen
+                        });
+                }
+            }
+            var existenciasCC = db.Set<ExistenciaCentroDeCosto>().ToList();
+            foreach (var existencia in existenciasCC)
+            {
+                if (existencia.Cantidad > 0)
+                {
+                    db.Set<MovimientoDeProducto>().Add(new MovimientoDeProducto()
+                    {
+                        Fecha = DateTime.Now,
+                        DiaContable = dia,
+                        CentroDeCostoId = existencia.CentroDeCostoId,
+                        ProductoId = existencia.ProductoId,
+                        TipoId = db.Set<TipoDeMovimiento>().SingleOrDefault(t => t.Descripcion == TipoDeMovimientoConstantes.Entrada).Id,
+                        Cantidad = existencia.Cantidad,
+                    });
+                }
+            }
+
+            db.SaveChanges();
+
+            TempData["exito"] = "Todo borrado";
+            return RedirectToAction("Index", "Inicio");
+        }
+    }
+}
