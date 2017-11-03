d83053eb82c753bce60308a6eeae7a6d2c0080f6
elvis <elvis.crego@gmail.com>
2017-11-03 09:52:44 -0400

    Agregando proyecto de pruebas

---
+++ ErpMvcTest/VentasTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using CompraVentaCore.Models;
+using ContabilidadCore.Models;
+using ErpMvc.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SeguridadCore.Models;
+
+namespace ErpMvcTest
+{
+    [TestClass]
+    public class VentasTest
+    {
+        [TestMethod]
+        public void LoadTest()
+        {
+            var db = new ErpContext();
+            var dia = new DiaContable() {Fecha = DateTime.Now,Abierto = true};
+            db.Set<DiaContable>().Add(dia);
+
+            var user = new Usuario() {UserName = "pepe"};
+            db.SaveChanges();
+
+            var venta = new Venta() {Fecha = DateTime.Now, DiaContable = dia,Elaboraciones = new List<DetalleDeVenta>() {new DetalleDeVenta() {ElaboracionId = 50,Cantidad = 2} } };
+        }
+    }
+}
