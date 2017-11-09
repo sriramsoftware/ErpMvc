2e828d2e97ab035a337b116d587834171c93b48f
elvis <elvis.crego@gmail.com>
2017-11-08 23:51:29 -0500

    Agregando gestion de productos concretos en area de admin

---
+++ ErpMvc/Areas/Admin/Controllers/ProductoConcretoController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AlmacenCore.Models;
+using ErpMvc.Models;
+
+namespace ErpMvc.Areas.Admin.Controllers
+{
+    [Authorize(Roles = RolesMontin.Administrador)]
+    public class ProductoConcretoController : Controller
+    {
+        private DbContext _db;
+
+        public ProductoConcretoController(DbContext context)
+        {
+            _db = context;
+        }
+
+        // GET: Admin/ExistenciaAlmacen
+        public ActionResult Index()
+        {
+            return View(_db.Set<ProductoConcreto>().ToList());
+        }
+
+        
+        // GET: Admin/ExistenciaAlmacen/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var exist = _db.Set<ExistenciaAlmacen>().Find(id);
+            return View(exist);
+        }
+
+        // POST: Admin/ExistenciaAlmacen/Edit/5
+        [HttpPost]
+        public ActionResult Edit(ExistenciaAlmacen existenciaAlmacen)
+        {
+            try
+            {
+                var exist = _db.Set<ExistenciaAlmacen>().Find(existenciaAlmacen.Id);
+                exist.ExistenciaEnAlmacen = existenciaAlmacen.ExistenciaEnAlmacen;
+                _db.Entry(exist).State = EntityState.Modified;
+                _db.SaveChanges();
+                TempData["exito"] = "Existencia modificada correctamente";
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ErpMvc/Areas/Admin/Models/EliminarExistenciaViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ErpMvc.Areas.Admin.Models
+{
+    public class EliminarExistenciaViewModel
+    {
+        public int Id { get; set; }
+
+        public int CentroId { get; set; } 
+    }
+}
