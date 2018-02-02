614be4717ea328d912cfa819343417caa4f6a6f1
elvis <elvis.crego@gmail.com>
2018-02-01 21:47:50 -0500

    Agregando modelos para seleccion de ventas y compras

---
+++ ErpMvc/Models/SeleccionCompra.cs
@@ -0,0 +1,13 @@
+﻿using CompraVentaCore.Models;
+
+namespace ErpMvc.Models
+{
+    public class SeleccionCompra
+    {
+        public int Id { get; set; }
+
+        public int CompraId { get; set; }
+
+        public virtual Compra Compra { get; set; } 
+    }
+}
---
+++ ErpMvc/Models/SeleccionVenta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CompraVentaCore.Models;
+
+namespace ErpMvc.Models
+{
+    public class SeleccionVenta
+    {
+        public int Id{ get; set; }
+
+        public int VentaId { get; set; }
+
+        public virtual Venta Venta { get; set; }
+    }
+}
