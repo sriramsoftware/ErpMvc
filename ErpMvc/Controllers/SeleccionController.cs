32520be7f239829e57c2519107dd0fa904b33696
elvis <elvis.crego@gmail.com>
2018-02-10 08:58:08 -0500

    Agregando seleccion de compras

---
+++ ErpMvc/Controllers/SeleccionController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CompraVentaCore.Models;
+using ContabilidadBL;
+using ContabilidadCore.Models;
+using ErpMvc.Models;
+using ErpMvc.ViewModels;
+
+namespace ErpMvc.Controllers
+{
+    public class SeleccionController : Controller
+    {
+        private DbContext _db;
+        private PeriodoContableService _periodoContableService;
+
+        public SeleccionController(DbContext context)
+        {
+            _db = context;
+            _periodoContableService = new PeriodoContableService(context);
+        }
+
+        // GET: Seleccion
+        public ActionResult Ventas()
+        {
+            var ventas = _db.Set<SeleccionVenta>().Include(v => v.Venta).ToList();
+            return View(ventas);
+        }
+
+        public ActionResult SeleccionarVentas(int id)
+        {
+            return View();
+        }
+
+        public ActionResult Compras()
+        {
+            var compras = _db.Set<Compra>().GroupBy(c => c.DiaContable).Select(g => new ComprasSeleccionViewModel()
+            {
+                DiaContable = g.Key,
+                Importe = g.Sum(co => co.Productos.Sum(p => p.ImporteTotal))
+            });
+            return View(compras);
+        }
+        
+        public ActionResult SeleccionarCompras(int id)
+        {
+            var compras = _db.Set<Compra>().Where(c => c.DiaContableId == id);
+            return View(compras);
+        }
+
+        [HttpPost]
+        public ActionResult SeleccionarCompras(List<int> comprasIds )
+        {
+            try
+            {
+                foreach (var id in comprasIds)
+                {
+                    _db.Set<SeleccionCompra>().Add(new SeleccionCompra() {CompraId = id});
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Compras");
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ErpMvc/ViewModels/ComprasSeleccionViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ContabilidadCore.Models;
+
+namespace ErpMvc.ViewModels
+{
+    public class ComprasSeleccionViewModel
+    {
+        public DiaContable DiaContable { get; set; }
+
+        public decimal Importe { get; set; }
+    }
+}
